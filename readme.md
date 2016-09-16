9011d1b0515d53aadeca69210bffc18e1a8c5a54
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-16 18:17:10 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+Weekend 4 Challenge
+==================
+
+Create a To-Do List App
+------------------------
+
+Technology:
+* SQL
+* Node.js
+* Express
+* Postgress
+* JQuery
---
+++ server/app.js
@@ -0,0 +1,31 @@
+var express = require( 'express' );
+var app = express();
+var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
+var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var port = process.env.PORT || 8080;
+
+// static public folder
+app.use( express.static( 'public' ) );
+
+// spin up server
+app.listen( port, function(){
+  console.log( 'server up on', port );
+});
+
+// base
+app.get( '/', function( req, res ){
+  console.log( 'base URL hit' );
+  res.sendFile( 'index.html' );
+}); // end base url
+
+// post template
+app.post( '/poster', urlencodedParser, function( req, res ){
+  console.log( 'in post template', req.body );
+  // assemble objectToSend
+  var objectToSend={
+    response: 'from server'
+  };
+  // send object back to client
+  res.send( objectToSend );
+}); // end post template
