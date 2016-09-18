38065159fe426313975fb8e146d91df40cbeabee
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-18 10:41:34 -0500

    backend almost working

--- server/app.js
+++ server/app.js
@@ -1,31 +1,103 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres:localhost:5432/to-do-list';
 var port = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // static public folder
-app.use( express.static( 'public' ) );
+app.use(express.static('public'));
 
 // spin up server
-app.listen( port, function(){
-  console.log( 'server up on', port );
+app.listen(port, function () {
+  console.log('spinning on', port);
 });
 
-// base
-app.get( '/', function( req, res ){
-  console.log( 'base URL hit' );
-  res.sendFile( 'index.html' );
+/////////////////////////////// BASE URL //////////////////////////////////
+app.get('/', function (req, res) {
+  console.log('base URL hit');
+  res.sendFile('index.html');
 }); // end base url
 
-// post template
-app.post( '/poster', urlencodedParser, function( req, res ){
-  console.log( 'in post template', req.body );
-  // assemble objectToSend
-  var objectToSend={
-    response: 'from server'
-  };
-  // send object back to client
-  res.send( objectToSend );
-}); // end post template
+// list route url
+app.route('/list')
+
+/////////////////////////////// GET ///////////////////////////////////////
+  .get(function (req, res) {
+    console.log('/list get response', req.body);
+    pg.connect(connectionString, function (err, client, done) {
+        if (err) res.status(500).send('Connection error');
+
+        var resultsArray = [];
+
+        var query = client.query('SELECT * FROM list ORDER BY id ASC');
+        query.on('row', function (row) {
+          resultsArray.push(row);
+        });
+
+        query.on('end', function () {
+          done();
+          return res.send(resultsArray);
+        });
+      }); // end pg connect
+  }) // end /list get
+
+//////////////////////////////// POST //////////////////////////////////////////////
+  .post(function (req, res) {
+    console.log('in post', req.body);
+
+    var data = req.body;
+    console.log('data:', data);
+
+    pg.connect(connectionString, function (err, client, done) {
+
+      // check for connection error
+
+      if (err) res.status(500).send('Connection error');
+
+      // query database to add/post/insert a tast in the list table
+      var query = client.query('INSERT INTO "list" ("name", "status") VALUES ($1, false);' [data.name, data.status]);
+
+      // push each row of DB TABLE list into resultsArray
+      query.on('row', function (row) {
+        resultsArray.push(row);
+        console.log(resultsArray);
+      });
+
+      query.on('end', function () {
+        done();
+        return res.send('post success!');
+      }); // end of on end function
+    }); // end pg connect
+  }) // end post call
+
+  //////////////////////////////// PUT //////////////////////////////////////////////
+  .put(function (req, res) {
+
+    // Body Data
+    var data = req.body;
+
+    console.log('Data Recieved:', data);
+
+    pg.connect(connectionString, function (err, client, done) {
+
+      // log/send error if ture
+      if (err) res.status(500).send('Connection error');
+      var resultsArray = [];
+      var query = client.query('DELETE FROM list WHERE id=($1)', [data.id]);
+
+      query.on('row', function (row) {
+        resultsArray.push(row);
+      }); // end on row
+
+      query.on('end', function () {
+        done();
+        return res.status(200).send({ status: 'Working' });
+      }); // end on end
+    }); // end pg connect
+  }); // end put delete task
